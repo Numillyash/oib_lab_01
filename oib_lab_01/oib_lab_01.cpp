174ac1352f8d5eeba4426cae75ba5f2055573ec7
Numillyah <Numillyash-std@yandex.ru>
2022-02-10 13:50:54 +0300

    Реализован алгоритм Евклида и задача 3

--- oib_lab_01/oib_lab_01.cpp
+++ oib_lab_01/oib_lab_01.cpp
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <conio.h>
 
+//#define DEBUG
 #define elif else if
 
 int N_gr = 3; // Номер группы
@@ -35,7 +36,7 @@ void task2(const char* string, int m, int k)
     int numb, rot_numb;
     char rot_symb;
 
-    for (int i = 0; i < length - 1; i++)
+    for (int i = 0; i < length; i++)
     {
         if (string[i] >= 'А' && string[i] <= 'Я')
         {
@@ -57,14 +58,71 @@ void task2(const char* string, int m, int k)
     printf("\n");
 }
 
+/// <summary>
+/// Алгоритм Евклида для двух целых чисел
+/// </summary>
+/// <param name="a">Первое число</param>
+/// <param name="b">Второе число</param>
+/// <returns>НОД трех чисел</returns>
+int euclide_algorithm(int a, int b)
+{
+    if (a < b)
+    {
+        int _ = b;
+        b = a;
+        a = _;
+    }
+    //a = b * q_0 + r_1
+    int r = a % b;
+    #ifdef DEBUG
+    printf("%d = %d * %d + %d\n", a, b, a/b, a%b);
+    #endif // DEBUG
+
+    if (r != 0)
+        euclide_algorithm(b, r);
+    else
+        return b;
+}
+
+/// <summary>
+/// Алгоритм Евклида для трех целых чисел
+/// </summary>
+/// <param name="a">Первое число</param>
+/// <param name="b">Второе число</param>
+/// <param name="c">Третье число</param>
+/// <returns>НОД трех чисел</returns>
+int euclide_algorithm(int a, int b, int c)
+{
+    int o_del = euclide_algorithm(a, b);
+    if (o_del == 1)
+        return 1;
+    else
+        return euclide_algorithm(c, o_del);
+}
+
+/// <summary>
+/// Подпрограмма, выполняющая задачу 3
+/// </summary>
+void task3()
+{
+    int A = pow(N_gr * (8 + N_sp % 7),2);
+    int B = 11032004;
+    //printf("%d\n", r1);
+
+    printf("%d\n", euclide_algorithm(A, (B % 95) + 900));
+    printf("%d\n", euclide_algorithm(A, ((B + 50) % 97) + 700));
+    printf("%d\n", euclide_algorithm(A, ((B + 20) % 101) + 1500, ((B -40) % 103) + 2500));
+}
+
 int main() {
     setlocale(LC_ALL, "rus");
 
     task1();
 
     char fio[] = "Улановский Георгий Алексеевич";
-    task2(fio, 33, 3);
+    task2(fio, 32, 3);
 
+    task3();
     _getch();
     return 0;
 }
